fbc064d424a6db6ce17e426cac6bd82b16dd9590
Zarapwa <doulatshahiinfo@gmail.com>
2025-12-06 06:31:26 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "exchange-v103";
+const CACHE_NAME = "exchange-v104";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
@@ -22,4 +22,5 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
 
